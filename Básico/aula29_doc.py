c350c3e76149790271747f52ad3cfdc810a2e87a
Rodrigo <rsb6@cin.ufpe.br>
2023-07-08 14:24:53 -0300

    initial commit

---
+++ Básico/aula29_doc.py
@@ -0,0 +1,10 @@
+# DOCUMENTAÇÃO EM PYTHON
+#  https://docs.python.org/pt-br/3/tutorial/index.html
+
+teste = [1,5,6,8,9,1]
+
+f = input("alguma coisa: ")
+index = None
+if f in teste:
+    index = teste.index()
+print(index)
